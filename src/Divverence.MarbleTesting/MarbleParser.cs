613a9aeac744a580b9482263fbece685c1493ad4
Bart de Boer <bart.de.boer@divverence.com>
2016-11-25 11:33:43 +0100

    Added support for ( groups ) in marble strings. Added support for spaces (totally ignored) and start/subscription-markers (shifting time)

--- src/Divverence.MarbleTesting/MarbleParser.cs
+++ src/Divverence.MarbleTesting/MarbleParser.cs
@@ -1,11 +1,81 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Divverence.MarbleTesting
 {
     public static class MarbleParser
     {
-        public static IEnumerable<Moment> ParseMarbles(string line) => line.Select(
-            (character, time) => character == '-' ? new Moment(time) : new Moment(time, character.ToString()));
+        public static IEnumerable<Moment> ParseMarbles(string line)
+        {
+            if (line == null)
+                throw new ArgumentNullException(nameof(line));
+            int? timeOffset;
+            var moments = ParseMarbles(line, out timeOffset);
+            if (!timeOffset.HasValue || timeOffset.Value == 0)
+                return moments;
+            return moments.Select(m => new Moment(m.Time + timeOffset.Value, m.Marbles));
+        }
+
+        public static List<Moment> ParseMarbles(string line, out int? timeOffset)
+        {
+            var retVal = new List<Moment>();
+            var time = 0;
+            int groupTime = 0;
+            timeOffset = null;
+            List<string> groupMarbles = null;
+            foreach (var character in line)
+            {
+                if (groupMarbles == null)
+                switch (character)
+                {
+                    case ' ':
+                        continue;
+                    case '-':
+                        retVal.Add(new Moment(time));
+                        break;
+                    case '^':
+                        if (timeOffset.HasValue)
+                            throw new ArgumentException("Only one ^ character allowed", nameof(line));
+                        timeOffset = -time;
+                        retVal.Add(new Moment(time, character.ToString()));
+                        break;
+                    case '(':
+                        groupTime = time;
+                        groupMarbles = new List<string>();
+                        break;
+                    case ')':
+                        throw new ArgumentException("Closing parentheses without opening parentheses", nameof(line));
+                    default:
+                        retVal.Add(new Moment(time, character.ToString()));
+                        break;
+                }
+                else switch (character)
+                {
+                    case ' ':
+                        continue;
+                    case '-':
+                            throw new ArgumentException("Cannot use - within a group", nameof(line));
+                    case '^':
+                        if (timeOffset.HasValue)
+                            throw new ArgumentException("Only one ^ character allowed", nameof(line));
+                        timeOffset = -groupTime;
+                        groupMarbles.Add(character.ToString());
+                        break;
+                    case '(':
+                        throw new ArgumentException("Cannot have nested parentheses", nameof(line));
+                    case ')':
+                        retVal.Add(new Moment(groupTime, groupMarbles.ToArray()));
+                        groupMarbles = null;
+                        break;
+                    default:
+                        groupMarbles.Add(character.ToString());
+                        break;
+                }
+
+                time++;
+            }
+            return retVal;
+        }
     }
 }
--- tst/Divverence.MarbleTesting.Tests/MarbleParserTest.cs
+++ tst/Divverence.MarbleTesting.Tests/MarbleParserTest.cs
@@ -22,11 +22,102 @@ namespace Divverence.MarbleTesting.Tests
             parsingNull.ShouldThrow<ArgumentNullException>();
         }
 
+        [Theory]
+        [InlineData("((")]
+        [InlineData("())")]
+        [InlineData(")")]
+        public void Should_throw_ArgumentException_when_passing_wrong_parentheses( string marbleLine )
+        {
+            Action parsingNull = () => MarbleParser.ParseMarbles(marbleLine);
+            parsingNull.ShouldThrow<ArgumentException>();
+        }
+
+        [Theory]
+        [InlineData("^^")]
+        [InlineData("(^)^")]
+        [InlineData("(^)(^)")]
+        [InlineData("^(^)")]
+        [InlineData("(^^)")]
+        [InlineData("^abc^")]
+        public void Should_throw_ArgumentException_when_passing_two_starters(string marbleLine)
+        {
+            Action parsingNull = () => MarbleParser.ParseMarbles(marbleLine);
+            parsingNull.ShouldThrow<ArgumentException>();
+        }
+
+        [Theory]
+        [InlineData("(-)")]
+        [InlineData("(-^)")]
+        [InlineData("(^-)")]
+        [InlineData("(ab-)")]
+        [InlineData("ab(-b)")]
+        public void Should_throw_ArgumentException_when_passing_dash_in_group(string marbleLine)
+        {
+            Action parsingNull = () => MarbleParser.ParseMarbles(marbleLine);
+            parsingNull.ShouldThrow<ArgumentException>();
+        }
+
         [Fact]
         public void Should_have_time_increasing_from_0()
         {
             var actual = MarbleParser.ParseMarbles("abc");
             actual.Select(m => m.Time).Should().BeEquivalentTo(new[] {0, 1, 2});
+        }
+
+        [Fact]
+        public void Start_at_beginning_behaves_like_normal_marble()
+        {
+            var actual = MarbleParser.ParseMarbles("^");
+            actual.Should().HaveCount(1);
+            actual.Single().Time.Should().Be(0);
+            actual.Single().Marbles.Should().BeEquivalentTo("^");
+        }
+
+        [Fact]
+        public void Should_have_time_increasing_from_minus3()
+        {
+            var actual = MarbleParser.ParseMarbles("abc^def");
+            actual.Select(m => m.Time).Should().BeEquivalentTo(new[] { -3, -2, -1, 0, 1, 2, 3 });
+        }
+
+        [Fact]
+        public void Should_have_time_increasing_from_minus2()
+        {
+            var actual = MarbleParser.ParseMarbles("ab(c^d)ef");
+            actual.Select(m => string.Concat(m.Marbles)).Should().BeEquivalentTo("a", "b", "c^d", "e", "f");
+            actual.Select(m => m.Time).Should().BeEquivalentTo(new[] { -2, -1, 0, 5, 6 });
+        }
+
+        [Fact]
+        public void Supports_group_in_middle()
+        {
+            var actual = MarbleParser.ParseMarbles("ab(cd)ef");
+            actual.Select(m => string.Concat(m.Marbles)).Should().BeEquivalentTo("a", "b", "cd", "e", "f");
+            actual.Select(m => m.Time).Should().BeEquivalentTo(new[] {0, 1, 2, 6, 7});
+        }
+
+        [Fact]
+        public void Supports_multiple_groups()
+        {
+            var actual = MarbleParser.ParseMarbles("ab(cd)ef(gh)");
+            actual.Select(m => string.Concat(m.Marbles)).Should().BeEquivalentTo("a", "b", "cd", "e", "f", "gh");
+            actual.Select(m => m.Time).Should().BeEquivalentTo(new[] { 0, 1, 2, 6, 7, 8 });
+        }
+
+        [Fact]
+        public void Supports_group_at_start()
+        {
+            var actual = MarbleParser.ParseMarbles("(cd)ef");
+            actual.Select(m => string.Concat(m.Marbles)).Should().BeEquivalentTo("cd", "e", "f");
+            actual.Select(m => m.Time).Should().BeEquivalentTo(new[] { 0, 4, 5 });
+        }
+
+        [Fact]
+        public void Supports_group_at_end()
+        {
+            var actual = MarbleParser.ParseMarbles("ab(cd)");
+            actual.Select(m => string.Concat(m.Marbles)).Should().BeEquivalentTo("a", "b", "cd");
+            actual.Select(m => m.Time).Should().BeEquivalentTo(new[] { 0, 1, 2 });
         }
 
         [Fact]
@@ -58,6 +149,23 @@ namespace Divverence.MarbleTesting.Tests
             actual.Should().HaveCount(2);
             actual.First().Marbles.Should().BeEmpty();
             actual.Skip(1).First().Marbles.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void Should_use_single_moment_for_all_grouped_marbles()
+        {
+            var actual = MarbleParser.ParseMarbles("(abc)");
+            actual.Select(m => m.Time).Should().BeEquivalentTo(new[] { 0 });
+            actual.Single().Marbles.Should().BeEquivalentTo("a", "b", "c");
+        }
+
+        [Fact]
+        public void Should_ignore_spaces()
+        {
+            var actual = MarbleParser.ParseMarbles("   a b  c -  (de)   f  ");
+            var expected = MarbleParser.ParseMarbles("abc-(de)f");
+            actual.Select(m => m.Time).Should().BeEquivalentTo(expected.Select(m => m.Time));
+            actual.Select(m => string.Concat(m.Marbles)).Should().BeEquivalentTo(expected.Select(m => string.Concat(m.Marbles)));
         }
     }
 }
