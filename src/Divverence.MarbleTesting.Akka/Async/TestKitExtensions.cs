61655f88abff8c0fbca7d4500d15754c4e5497c8
Bart de Boer <bart.de.boer@divverence.com>
2016-11-28 12:35:18 +0100

    Added TestProbeExtensions, Added TestKitExtensions and optional hint messages

--- src/Divverence.MarbleTesting.Akka/Async/TestKitExtensions.cs
+++ src/Divverence.MarbleTesting.Akka/Async/TestKitExtensions.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading.Tasks;
+using Akka.Actor;
 using Akka.TestKit;
 
 namespace Divverence.MarbleTesting.Akka.Async
@@ -9,22 +10,34 @@ namespace Divverence.MarbleTesting.Akka.Async
     /// </summary>
     public static class TestKitExtensions
     {
-        public static async Task ExpectNoMsgAsync( this TestKitBase testKit )
+        public static async Task ExpectNoMsgAsync(this TestKitBase testKit)
         {
             await testKit.Sys.Idle();
             testKit.ExpectNoMsg(TimeSpan.Zero);
         }
 
-        public static async Task<T> ExpectMsgAsync<T>(this TestKitBase testKit)
+        public static async Task<T> ExpectMsgAsync<T>(this TestKitBase testKit, string hint = null)
         {
             await testKit.Sys.Idle();
-            return testKit.ExpectMsg<T>(TimeSpan.Zero);
+            return testKit.ExpectMsg<T>(TimeSpan.Zero, hint);
         }
 
-        public static async Task<T> ExpectMsgAsync<T>(this TestKitBase testKit, Predicate<T> predicate)
+        public static async Task<T> ExpectMsgAsync<T>(this TestKitBase testKit, Predicate<T> predicate, string hint = null)
         {
             await testKit.Sys.Idle();
-            return testKit.ExpectMsg(predicate, TimeSpan.Zero);
+            return testKit.ExpectMsg(predicate, TimeSpan.Zero, hint);
+        }
+
+        public static async Task<T> ExpectMsgAsync<T>(this TestKitBase testKit, Action<T> assertion, string hint = null)
+        {
+            await testKit.Sys.Idle();
+            return testKit.ExpectMsg(assertion, TimeSpan.Zero, hint);
+        }
+
+        public static async Task ExpectTerminatedAsync<T>(this TestKitBase testKit, IActorRef actorRef, string hint = null)
+        {
+            await testKit.Sys.Idle();
+            testKit.ExpectTerminated(actorRef, TimeSpan.Zero, hint);
         }
     }
 }
---
+++ src/Divverence.MarbleTesting.Akka/Async/TestProbeExtensions.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Akka.TestKit;
+
+namespace Divverence.MarbleTesting.Akka.Async
+{
+    /// <summary>
+    /// Incomplete set of helpers that help write reliable, fast, asynchronous actor tests.
+    /// </summary>
+    public static class TestProbeExtensions
+    {
+        public static async Task ExpectNoMsgAsync(this TestProbe testProbe)
+        {
+            await testProbe.Sys.Idle();
+            testProbe.ExpectNoMsg(TimeSpan.Zero);
+        }
+
+        public static async Task<T> ExpectMsgAsync<T>(this TestProbe testProbe, string hint = null)
+        {
+            await testProbe.Sys.Idle();
+            return testProbe.ExpectMsg<T>(TimeSpan.Zero, hint);
+        }
+
+        public static async Task<T> ExpectMsgAsync<T>(this TestProbe testProbe, Predicate<T> predicate, string hint = null)
+        {
+            await testProbe.Sys.Idle();
+            return testProbe.ExpectMsg(predicate, TimeSpan.Zero, hint);
+        }
+
+        public static async Task<T> ExpectMsgAsync<T>(this TestProbe testProbe, Action<T> assertion, string hint = null)
+        {
+            await testProbe.Sys.Idle();
+            return testProbe.ExpectMsg(assertion, TimeSpan.Zero, hint);
+        }
+
+        public static async Task ExpectTerminatedAsync<T>(this TestProbe testProbe, IActorRef actorRef, string hint = null)
+        {
+            await testProbe.Sys.Idle();
+            testProbe.ExpectTerminated(actorRef, TimeSpan.Zero, hint);
+        }
+    }
+}
