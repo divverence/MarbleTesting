95e961f528b38b6f90eacab85fa2c0e0ac0c7110
Bart de Boer <bart.de.boer@divverence.com>
2016-11-28 09:38:29 +0100

    Removed duplicate Copyright, Company attrs

--- src/Divverence.MarbleTesting/Properties/AssemblyInfo.cs
+++ src/Divverence.MarbleTesting/Properties/AssemblyInfo.cs
@@ -1,6 +1,4 @@
 ﻿using System.Reflection;
 [assembly: AssemblyTitle("Divverence.MarbleTesting")]
 [assembly: AssemblyDescription("Divverence.MarbleTesting Core Library")]
-[assembly: AssemblyCompany("Divverence B.V.")]
-[assembly: AssemblyCopyright("Copyright © 2016 Divverence B.V.")]
 
