b126ae3ebb75884e62abc7e79f251c35cf19fb12
Bart de Boer <bart.de.boer@divverence.com>
2017-12-04 10:10:12 +0100

    Added extension methods for pooling true async tasks on Awaitable TaskDispatcher

---
+++ src/Divverence.MarbleTesting.Akka/Async/AwaitableTaskDispatcherRelatedExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Akka.Actor;
+
+namespace Divverence.MarbleTesting.Akka.Async
+{
+    public static class AwaitableTaskDispatcherRelatedExtensions
+    {
+        public static Task TrackOnDispatcher(this Task task, AwaitableTaskDispatcher dispatcherOrNull)
+        {
+            dispatcherOrNull?.PoolTask(task);
+            return task;
+        }
+
+        public static Task TrackOnDispatcher(this Task task, IActorContext context) => task.TrackOnDispatcher(context.Dispatcher as AwaitableTaskDispatcher);
+
+        public static Task TrackTaskOnDispatcher(this IActorContext context, Task task) => task.TrackOnDispatcher(context.Dispatcher as AwaitableTaskDispatcher);
+    }
+}
